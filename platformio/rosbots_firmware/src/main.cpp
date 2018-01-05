78056c942cb1da0a26e0dda660ddd7f7e7124b63
Jack Pien <jack@rosbots.com>
2018-01-05 15:35:50 -0800

    Add some code to process Twist message in the UNO

--- platformio/rosbots_firmware/src/main.cpp
+++ platformio/rosbots_firmware/src/main.cpp
@@ -9,10 +9,13 @@
 #include "Arduino.h"
 
 #include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
 
 ros::NodeHandle nh;
 
 std_msgs::String str_msg;
+geometry_msgs::Twist twist_msg;
+
 ros::Publisher chatter("chatter", &str_msg);
 
 char hello[13] = "hello world!";
@@ -20,6 +23,15 @@ char hello[13] = "hello world!";
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
+
+char twist_rcv[15] = "twist received";
+
+void twistCb( const geometry_msgs::Twist &twist_msg ) {
+  str_msg.data = twist_rcv;
+  chatter.publish( &str_msg );
+}
+ros::Subscriber<geometry_msgs::Twist> sub("twist", &twistCb );
+
 
 void setup()
 {
