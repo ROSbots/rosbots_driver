415fe817801eccc286d8574e507e85e339cc090b
Jack Pien <jack@rosbots.com>
2018-02-17 01:34:43 +0000

    Add example scripts and firmware for the wheel encoders

---
+++ platformio/rosbots_firmware/examples/motor_driver_wheel_encoders/src/main.cpp
@@ -0,0 +1,128 @@
+/**
+ * Motor Driver Example
+ */
+
+#include <ros.h>
+#include "Arduino.h"
+
+#include <std_msgs/UInt32.h>
+#include <std_msgs/Float32.h>
+
+ros::NodeHandle nh;
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+#define M_LEFT_PWM 6
+#define M_LEFT_FR 7
+#define M_RIGHT_PWM 5
+#define M_RIGHT_FR 4
+
+void turnWheel( const std_msgs::Float32 &wheel_power,
+                unsigned int pwm_pin,
+                unsigned int fr_pin ) {
+    float factor = max(min(wheel_power.data, 1.0f), -1.0f);
+    if( factor >= 0 ) {
+        digitalWrite(fr_pin, LOW);
+        analogWrite(pwm_pin, (unsigned int)(255 * factor));
+    } else {
+        digitalWrite(fr_pin, HIGH);
+        analogWrite(pwm_pin, (unsigned int)(255 * (1.0f + factor)));
+    }   
+}
+void rightWheelCb( const std_msgs::Float32 &wheel_power ) {
+    turnWheel( wheel_power, M_RIGHT_PWM, M_RIGHT_FR );
+}
+void leftWheelCb( const std_msgs::Float32 &wheel_power ) {
+    turnWheel( wheel_power, M_LEFT_PWM, M_LEFT_FR );
+}
+ros::Subscriber<std_msgs::Float32> sub_right("wheel_power_right",
+                                            &rightWheelCb );
+ros::Subscriber<std_msgs::Float32> sub_left("wheel_power_left",
+                                           &leftWheelCb );
+
+volatile unsigned int rwheel=0;
+volatile unsigned long rdebounce=0;
+volatile unsigned int lwheel=0;
+volatile unsigned long ldebounce=0;
+
+#define DEBOUNCE_MS 1500 // in micro-sec
+
+void lspeed() {
+    // N microseconds since last interrupt
+    if( micros() - ldebounce > DEBOUNCE_MS ){ 
+        ldebounce = micros();
+        lwheel = lwheel + 1;
+    }
+}
+
+void rspeed() {
+    // N microseconds since last interrupt
+    if( micros() - rdebounce > DEBOUNCE_MS ){
+        rdebounce = micros();
+        rwheel = rwheel + 1;
+    }
+}
+
+std_msgs::UInt32 rticks_msg;
+std_msgs::UInt32 lticks_msg;
+ros::Publisher rticks_pub("wheel_ticks_right", &rticks_msg);
+ros::Publisher lticks_pub("wheel_ticks_left", &lticks_msg);
+
+void setup()
+{
+    // Set up wheel encoder interrupts
+    pinMode(2, INPUT);
+    attachInterrupt(digitalPinToInterrupt(2), rspeed, CHANGE);
+    pinMode(3, INPUT);
+    attachInterrupt(digitalPinToInterrupt(3), lspeed, CHANGE);
+    
+    // initialize LED digital pin as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    pinMode(M_LEFT_PWM, OUTPUT);
+    pinMode(M_LEFT_FR, OUTPUT);
+    pinMode(M_RIGHT_PWM, OUTPUT);
+    pinMode(M_RIGHT_FR, OUTPUT);
+    
+    // Init motors to stop
+    digitalWrite(M_LEFT_FR, LOW);
+    digitalWrite(M_RIGHT_FR, LOW);
+    analogWrite(M_LEFT_PWM, 0);
+    analogWrite(M_RIGHT_PWM, 0);
+    
+    nh.initNode();
+    nh.subscribe(sub_right);
+    nh.subscribe(sub_left);
+    
+    nh.advertise(rticks_pub);
+    nh.advertise(lticks_pub);
+}
+
+unsigned long last_tick_publish_ms = 0;
+void loop()
+{
+    unsigned int rw = rwheel;
+    unsigned int lw = lwheel;
+
+    // Only publish once per second if tick count has not
+    // changed
+    if( millis() >= last_tick_publish_ms + 1000 ||
+        rticks_msg.data != rw ||
+        lticks_msg.data != lw ) {
+        // Write out wheel encoder ticks
+        rticks_msg.data = rw;
+        rticks_pub.publish(&rticks_msg);
+        lticks_msg.data = lw;
+        lticks_pub.publish(&lticks_msg);
+        last_tick_publish_ms = millis();
+    } 
+    
+  //nh.loginfo("Log Me");
+  nh.spinOnce();
+
+  // wait for a second
+  //delay(1000);
+}
---
+++ scripts/examples/wheel_drive_01.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+#
+# This file is part of ROSbots ROS Drivers.
+#
+# Copyright
+#
+#     Copyright (C) 2017 Jack Pien <jack@rosbots.com>
+#
+# License
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Lesser General Public License as published
+#     by the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details at
+#     <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>
+#
+# Documentation
+#
+#     http://www.rosbots.com
+#
+# This module pulses the power to a 5v power bank using a 2222 NPN transistor
+# to prevent the power bank from shutting down due to inactivity. This allows
+# us to use the power bank to power the wheels in place of the 4xAA battery
+# pack. But since the wheels are not powered all the time, without this pulse
+# module, the power bank will just shut itself off.
+#
+
+import math
+
+import rospy
+from std_msgs.msg import UInt32, Float32
+
+
+class WheelDrive:
+    def __init__(self):
+        rospy.Subscriber("/wheel_ticks_right", UInt32,
+                         self.ticks_right_callback) 
+        self.pub_velocity_right = \
+            rospy.Publisher('/wheel_power_right', Float32, queue_size=10) 
+        rospy.on_shutdown(self.shutdown_cb)
+        self.wheel_power_right = Float32()
+        self.wheel_power_right.data = 0.0
+
+        self.ticks_right = None
+
+        self.stop_wheel_ticks = 40 * 3
+        
+    def ticks_right_callback(self, data):
+        #rospy.loginfo(rospy.get_caller_id() + " - ticks right: " + str(data.data))
+        if self.ticks_right == None:
+            self.ticks_right = UInt32()
+            self.ticks_right.data = data.data
+            rospy.loginfo(rospy.get_caller_id() + " INITIAL - ticks right: " + str(data.data))
+            
+        else:
+            if self.wheel_power_right.data != 0.0 and \
+               data.data >= self.ticks_right.data + self.stop_wheel_ticks:
+                self.wheel_power_right.data = 0.0
+                rospy.loginfo(rospy.get_caller_id() +
+                              " STOPPING - power right: " +
+                              str(self.wheel_power_right.data))
+                self.pub_velocity_right.publish(self.wheel_power_right)
+                rospy.loginfo(rospy.get_caller_id() +
+                              " STOPPING - ticks right: " + str(data.data))
+            elif data.data >= self.ticks_right.data + self.stop_wheel_ticks:
+                self.ticks_right.data = data.data
+
+    def run(self):
+        self.wheel_power_right.data = -1.0
+        self.pub_velocity_right.publish(self.wheel_power_right)
+
+    def shutdown_cb(self):
+        rospy.loginfo(rospy.get_caller_id() + " SHUTDOWN - ticks right: " + str(self.ticks_right.data))
+        
+        
+    
+def main():
+    rospy.init_node('wheel_drive_01', anonymous=False)
+    
+    wheel_drive = WheelDrive()
+    
+    rate = rospy.Rate(10)
+
+    once = 0
+    while not rospy.is_shutdown():
+
+        if once < 3:
+            wheel_drive.run()
+            once += 1
+        
+        #rospy.loginfo(rospy.get_caller_id() + ": Wake")
+        
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    main()
